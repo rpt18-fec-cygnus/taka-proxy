695c3514e415943cbaee27c2e138d6530fb5eba1
Takanori Sono <takanori.sono@gmail.com>
2020-02-04 21:39:08 -0800

    Main Info ajax request connects to service's API to load

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const {getRestaurantByID} = require('../InfoPage/mockData/dbHelper.js')
 
 app.use(express.static('public'));
 
-// app.get('/restaurantInfo', (req, res) => {
-//   // console.log(getRestaurantByID)
-//   getRestaurantByID(1, (data) => {
-//     res.send(data);
-//   })
-//   // res.send('fake data');
-// })
-
-// app.get('/restaurant/:restaurantId', (req, res) => {
-//   //req.params key value pair of restaurantID
-
-  
-//   // res.send(req.params.restaurantId)
-//   getRestaurantByID(req.params.restaurantId, (data) => {
-//     res.send(data);
-//   })
-//   // res.send('fake data');
-// })
+app.get('/restaurant/:restaurantId', (req, res) => {
+  getRestaurantByID(req.params.restaurantId, (data) => {
+    res.send(data);
+  })
+})
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
