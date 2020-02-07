8af77e85fa2dce24bf1b731a490ad0d00172189f
Takanori Sono <takanori.sono@gmail.com>
2020-02-06 23:44:16 -0800

    Serve static files from unique id url

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const {getRestaurantByID} = require('../InfoPage/mockData/dbHelper.js')
+const cors = require('cors')
 
-app.use(express.static('public'));
+app.use(cors());
 
-app.get('/restaurant/:restaurantId', (req, res) => {
-  getRestaurantByID(req.params.restaurantId, (data) => {
-    res.send(data);
-  })
-})
+app.use('/', express.static('public'));
+
+
+//Serve the same html page regardless of endpoint (for proxy)
+//individual services will use the url to make get request
+app.use('/restaurant/:restaurantId', express.static('public'));
+
+// app.get('/restaurant/:restaurantId', (req, res) => {
+//   getRestaurantByID(req.params.restaurantId, (data) => {
+//     res.send(data);
+//   })
+// });
+
+// app.get('/restaurant/:restaurantId', express.static('public'))
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
