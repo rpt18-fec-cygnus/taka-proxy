1c416ca316721af2d651f8d34611dd64abba47e7
Takanori Sono <takanori.sono@gmail.com>
2020-02-15 14:20:25 -0800

    Use proxy server for service get request and continue rendering static files

--- server.js
+++ server.js
@@ -3,22 +3,14 @@ const app = express();
 const port = 3000;
 const {getRestaurantByID} = require('../InfoPage/mockData/dbHelper.js')
 const cors = require('cors')
+const morgan = require('morgan');
+const proxy = require('express-http-proxy');
 
+
+app.use(morgan('tiny'));
 app.use(cors());
-
 app.use('/', express.static('public'));
-
-
-//Serve the same html page regardless of endpoint (for proxy)
-//individual services will use the url to make get request
 app.use('/restaurant/:restaurantId', express.static('public'));
-
-// app.get('/restaurant/:restaurantId', (req, res) => {
-//   getRestaurantByID(req.params.restaurantId, (data) => {
-//     res.send(data);
-//   })
-// });
-
-// app.get('/restaurant/:restaurantId', express.static('public'))
+app.get('/api/restaurant/:restaurantId', proxy('http://localhost:7000'));
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
