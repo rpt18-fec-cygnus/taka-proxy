0390a7d4d6df1bf969e295162bdb47343b7ef610
Takanori Sono <takanori.sono@gmail.com>
2020-02-18 19:38:18 -0800

    Add proxy endpoints for getting info by /restaurantName

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const proxy = require('express-http-proxy');
 app.use(morgan('tiny'));
 app.use(cors());
 app.use('/', express.static('public'));
+//serve static files for url endpoints
 app.use('/restaurant/:restaurantId', express.static('public'));
+app.use('/restaurantName/:restaurantName', express.static('public'));
+
+//proxy get requests to correct service
+//Info Service
 app.get('/api/restaurant/:restaurantId', proxy('http://localhost:7000'));
+app.get('/api/restaurantName/:restaurantName', proxy('http://localhost:7000'));
+
+//Reviews Service
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
