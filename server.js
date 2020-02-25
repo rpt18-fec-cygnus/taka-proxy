75ccc643b463ced217ee3f59aa671758847eac87
Takanori Sono <takanori.sono@gmail.com>
2020-02-24 23:04:31 -0800

    Create proxy for endpoints api/getScore and updateScore

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use('/restaurantName/:restaurantName', express.static('public'));
 //Info Service
 app.get('/api/restaurant/:restaurantId', proxy('http://localhost:7000'));
 app.get('/api/restaurantName/:restaurantName', proxy('http://localhost:7000'));
+app.patch('/api/updateScore', proxy('http://localhost:7000'));
 
 //Reviews Service
 app.get('/data', proxy('http://localhost:3001'));
+app.get('/api/getScore', proxy('http://localhost:3001'));
 app.post('/api/postReview', proxy('http://localhost:3001'));
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
